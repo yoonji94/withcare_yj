a68e640fd3f45aeb838cc19b4a4868fff2dd1f09
yoonji94 <125428002+yoonji94@users.noreply.github.com>
2023-03-24 13:07:47 +0900

    Add files via upload

---
+++ java/script333.js
@@ -0,0 +1,52 @@
+var slider1 = new Swiper ('.slider1', {
+    effect: 'slide',
+    
+    pagination: {
+      el: '.swiper-pagination',
+    },
+    
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+});
+
+
+var slider2 = new Swiper ('.slider2', {
+    effect: 'fade',
+    
+    pagination: {
+      el: '.swiper-pagination',
+    },
+    
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+});
+
+
+var slider3 = new Swiper ('.slider3', {
+    effect: 'cube',
+    
+    pagination: {
+      el: '.swiper-pagination',
+    },
+    
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+});
